bf1585f4149731670b145e327b844810564a4175
lucaslemhofer <lucasezequiellemhofer@gmail.com>
2026-05-07 18:19:13 -0300

    Implementación de taskName con Python 3.12 slim y librerias acorde.

--- clienteMqtt/clienteMqtt.py
+++ clienteMqtt/clienteMqtt.py
@@ -68,6 +68,6 @@ if __name__ == "__main__":
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        logging.info("Fin de la comunicación")
+        logging.info("Fin de la comunicación.")
         
         
